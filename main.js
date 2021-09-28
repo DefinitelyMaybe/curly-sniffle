f5c5f25517df9607ca21c4a6b836fc77323d2dfa
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-09-29 11:18:02 +1300

    blah

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
+  const response = new Response("Hello deno deploy!", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
