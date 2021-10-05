7e6f3ecb232f5d96a79f5983b41f390b7d996c4f
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-10-06 10:05:21 +1300

    create a build folder

--- build.ts
+++ build.ts
@@ -1,1 +1,8 @@
+import { emptyDirSync } from "https://deno.land/std@0.93.0/fs/mod.ts";
+
 console.log("Hello world");
+const p = `build`
+const f = `hello.txt`
+
+emptyDirSync(p)
+Deno.writeTextFileSync(`${p}/${f}`, "world")
